41b8d0b53e564f2f052de42ebf81a9abfda3ae0a
Lijing Wang <ooofreedom@gmail.com>
2015-03-17 16:11:41 -0700

    Update UBST

--- UBST.cpp
+++ UBST.cpp
@@ -12,13 +12,10 @@ public:
       b[1] = 1;
       for (int i = 2; i <= n; i ++)
       {
-        for (int j = 0; j < i/2; j ++)
+        for (int j = 0; j < i; j ++)
         {
           b[i] += b[j]*b[i-j-1];
         }
-        b[i] *=2;
-        if(i%2)
-          b[i] += b[i/2]*b[i/2];
       }
       return b[n];
     }
