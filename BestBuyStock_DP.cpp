9288fdc0ee3d03c4fb3661ef8303edb4c79c61b0
Lijing Wang <ooofreedom@gmail.com>
2015-03-17 10:29:58 -0700

    Best Sell Stock

--- BestBuyStock_DP.cpp
+++ BestBuyStock_DP.cpp
@@ -7,22 +7,23 @@ public:
     int maxProfit(vector<int> &prices) {
         
       if(prices.size()<2) return 0;
-      int i, max_whole = 0, cur = 0, diff;
-      for(i = 0; i < 
+      int i, pre_max = 0, all_max = 0, cur_max = 0, diff;
+
+      for(i = 0; i < prices.size()-1 ; i ++)
       {
         diff = prices[i+1] - prices[i];
-    
-        cur = max(cur+diff, 0);
-        max_whole = max(cur, max_whole);
+        cur_max = max(diff, pre_max + diff);
+        pre_max = cur_max;
+        all_max = max(cur_max, all_max);
       }
 
-      return max_whole; 
+      return all_max; 
     }
 };
 int main()
 {
-  int v[4] = {2,4,7,1};
-  std::vector<int> prices(&v[0], &v[3]);
+  int v[5] = {2,1,1,3,2};
+  std::vector<int> prices(&v[0], &v[5]);
   Solution so;
   std::cout << so.maxProfit(prices) << std::endl;
 }
