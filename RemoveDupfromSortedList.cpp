d1e36dfa8854fdaf7091ad425163962beaef942c
Lijing Wang <ooofreedom@gmail.com>
2015-03-24 11:34:22 -0700

    Add Remove Duplicates from Sorted List

---
+++ RemoveDupfromSortedList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+/* Two pointers Solution 
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (!head) return NULL;
+        ListNode *cur = head->next;
+        ListNode *pre = head;
+        while (cur) {
+            if (cur->val == pre->val) {
+                pre->next = cur->next;
+                delete cur;
+                cur = pre->next;
+            } else {
+                pre = pre->next;
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
+*/
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (!head) return head;
+        ListNode *cur = head;
+        while (cur->next) {
+            if (cur->val == cur->next->val) {
+                ListNode *del = cur->next;
+                cur->next = del->next;
+                delete(del);
+            } else {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
+
+int main()
+{
+}
